18b80bf27b145cd248114c587efb10f91dfd1378
Faldekk <kuba.mikula@wp.pl>
2026-06-25 00:09:01 +0200

    Added descriptions to proper parts for future users

---
+++ DrugCompare/Views/DataManagementView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace DrugCompare.Views;
+
+public partial class DataManagementView : UserControl
+{
+    public DataManagementView()
+    {
+        InitializeComponent();
+    }
+}
